ea0df466a6147d0dc57975ce15a6b95def1e43a6
lostdesign <info@lost.design>
2019-10-28 22:47:53 +0100

    :recycle: change headers on partners

--- src/views/Partners.vue
+++ src/views/Partners.vue
@@ -3,12 +3,12 @@ main
   section
     .content
       h1 Partners
-      h3 Domaincord
+      h2 Domaincord
       i tbd
       p
         a(href="https://domaincord.com/", title="Go to Domaincords Website").highlighted Domaincord
       br
-      h3 DesignDrop
+      h2 DesignDrop
       p Welcome to the one and only... DesignDrop! We're a community built up of creatives, who are looking to improve ourselves, meet new people, and have fun. Whether you're a beginner who just downloaded Adobe Illustrator, or a freelancer who has been working for years, we have a spot for you here. We encourage people to share their work and give valuable feedback.
       p Our server provides comfort, tips, and most importantly, community spirit! We are building our own marketplace where designers can upload and sell their work in a substantial and respectful way.
       p
