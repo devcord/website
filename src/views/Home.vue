42d17b3abdbef2a904f00487610c13bca02dec6b
lostdesign <info@lost.design>
2019-10-28 19:56:51 +0100

    :recycle: disable animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
 main
   section
-    TextWall
     .content
       h1 Looking for new friends?
       p Devcord is a community that brings together web developers of all experience levels. Get help with your code, connect with other web developers, discuss your current project and chat about your favourite languages, libraries, and frameworks.
