374145f8617845b8e77e51eeb865ecca4ae39cc9
Max <56621079+stark-inc@users.noreply.github.com>
2020-01-06 21:06:15 +0000

    scroll to top on route change

--- src/router/index.js
+++ src/router/index.js
@@ -60,7 +60,11 @@ const routes = [
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+      if (savedPosition) return savedPosition;
+      return { x: 0, y: 0 };
+  }
 });
 
 export default router;
