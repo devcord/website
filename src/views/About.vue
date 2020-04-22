173714384f5ce1933135f0e99944120e655911df
Gabe Dunn <gabe.dunn@shaw.ca>
2020-04-22 02:04:09 -0600

    make staff profile pictures static and add tags

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,10 +11,11 @@ main
         .card
           .card--meta
             .card--avatar
-              img(src="https://cdn.discordapp.com/avatars/126251022002814976/0427d645df822d8e466c033fd5639ace.png?size=128")
+              img(src="../assets/staff/lost.png")
             .card--name
               p.label.label--owner Owner
               h2 André Weller
+              p.label.label--tag lost#0001
           .card--details
             p André is a fullstack developer, based in Germany. His speciality is design and frontend with a strong background in Devops. He loves photography and cats.
             a(href="https://lost.design", title="André Weller's Website", target="_blank").highlighted Website
@@ -24,10 +25,11 @@ main
         .card
           .card--meta
             .card--avatar
-              img(src="https://cdn.discordapp.com/avatars/97466512444493824/42acc98a2e976e1df5450184450768cb.png?size=128")
+              img(src="../assets/staff/poisonapple.png")
             .card--name
               p.label.label--admin Admin
               h2 Andres Sweeney-Rios
+              p.label.label--tag Poison Apple#9351
           .card--details
             p Andres is a passionate frontend and game developer located in USA.
             a(href="https://poisonapple.dev", title="Andres Website", target="_blank").highlighted Website
@@ -35,39 +37,43 @@ main
         .card
           .card--meta
             .card--avatar
-              img(src="https://cdn.discordapp.com/avatars/170451883134156800/ea3f4eac0a645aeee3aea4a93d977867.png?size=128")
+              img(src="../assets/staff/redxtech.png")
             .card--name
               p.label.label--admin Admin
               h2 Gabe
+              p.label.label--tag RedXTech#5019
           .card-details
             p Gabe is a fullstack developer located in Canada. He specializes in vue.js and is studying software engineering. He is also the creator and maintainer of Devcords bot.
             a(href="https://gabedunn.dev", title="Gabe Dunn's Website", target="_blank").highlighted Website
         .card
           .card--meta
             .card--avatar
-              img(src="https://cdn.discordapp.com/avatars/169979689518104576/a_8dca3b19b90c1d625c35b7b439e153b9.png?size=128")
+              img(src="../assets/staff/pvthenk.png")
             .card--name
               p.label.label--mod Admin
               h2 James
+              p.label.label--tag pvtHenk#1337
           .card--details
             p As a Belgian fullstack web developer, Jannes mostly enjoys hosting knowledge transfers and exploring new technologies or languages. He can also often be found advocating the functional programming paradigm.
       .cards
         .card
           .card--meta
             .card--avatar
-              img(src="https://cdn.discordapp.com/avatars/206857563206189058/65518c51d124a81a157b22afa2723f78.png?size=128")
+              img(src="../assets/staff/q.png")
             .card--name
               p.label.label--mod Admin
               h2 ℚ.
+              p.label.label--tag ℚ.#7750
           .card--details
             p Austin is a computer science student and a freelance developer located in the United States. He works a lot with backend development and data visualization. As well works with teaching children and adults how to swim.
         .card
           .card--meta
             .card--avatar
-              img(src="https://cdn.discordapp.com/avatars/307247078885818368/583570dd3dd28ce61c5b50d58dafc103.png?size=128")
+              img(src="../assets/staff/polarbeer.png")
             .card--name
               p.label.label--mod Admin
               h2 polarbeer
+              p.label.label--tag polarbeer#0001
           .card--details
             p polarbeer is a designer and fullstack developer who has been involved in web since the dark ages of HTML 3 and inline CSS. Her interests include UI/UX, psychology, philosophy, fine art, graphic design, analytics, and engineering.
       .cards
@@ -160,6 +166,10 @@ section {
     font-weight: 300;
     text-transform: uppercase;
     letter-spacing: 5px;
+
+    &--tag {
+      font-size: 12px;
+    }
 
     &--owner {
       //background: #f0134d;
